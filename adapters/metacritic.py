941b96411c22933026c2aedb9c3fee97f504f3b2
Gabriel Peal <gpeal@u.northwestern.edu>
2013-03-12 14:25:17 -0500

    FEATURE: added metacritic adapter

--- adapters/metacritic.py
+++ adapters/metacritic.py
@@ -1,1 +1,26 @@
-from lib.meracritic import Metacritic
+from metacritic_lib import Metacritic
+from adapters import Adapter, FilmNotFoundError
+
+class MetaCriticAdapter(object):
+  def get_similar_film_titles(self, title):
+    meta = Metacritic()
+    movies = meta.search(title, 'movie')
+    if movies:
+      return [movie.title for movie in movies][:5]
+    else:
+        raise LookupError(title + ' not found in metacritic')
+
+  def get_film_score(self, title):
+    meta = Metacritic()
+    movies = meta.search(title, 'movie')
+    movie = [movie for movie in movies if movie.title == "Toy Story 2"][0]
+    import pdb; pdb.set_trace()
+    if movie:
+      return movie.metascore / 100.0
+    else:
+      raise FilmNotFoundError(title + ' not found on IMDb')
+
+  def __repr__(self):
+    return 'IMDb'
+
+Adapter.register(MetaCriticAdapter)
