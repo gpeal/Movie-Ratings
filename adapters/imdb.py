cf0ee4baed231aaab85c8ed582125aba647f4753
Gabriel Peal <gpeal@u.northwestern.edu>
2013-03-12 14:25:03 -0500

    BUG FIX: scale IMDb rating from 0-1

--- adapters/imdb.py
+++ adapters/imdb.py
@@ -15,7 +15,7 @@ class IMDbAdapter(object):
     r = requests.get('http://imdbapi.org/?q=%22' + title + '%22')
     movies = json.loads(r.text)
     if movies:
-      return movies[0]['rating']
+      return movies[0]['rating'] / 10.0
     else:
       raise FilmNotFoundError(title + ' not found on IMDb')
 
