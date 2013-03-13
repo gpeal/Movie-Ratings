c5b04e81104727d6c32418699a41ae156b77739f
Kalail <kalail@ubuntu.(none)>
2013-03-13 05:30:48 -0500

    REFACTOR: Improve adapter method.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,20 +1,20 @@
 
 
-def safe_find_film(films, title):
-	"""Finds the given film title in the films object"""
+def safe_find_film(film_title, film_titles):
+	"""Returns index of film with title film_title, in film_titles."""
 	# Setup check arrays
 	# this supports films being passed in as either a dict or an object
-	if isinstance(films[0], dict):
-		titles = [f.get('title', None) for f in films]
-	else:
-		titles = [f.title for f in films]
+	# if isinstance(films[0], dict):
+	# 	titles = [f.get('title', None) for f in films]
+	# else:
+	# 	titles = [f.title for f in films]
 
-	lowercase_titles = [t.lower() for t in titles]
+	lowercase_titles = [t.lower() for t in film_titles]
 	# Check simple
-	if title in titles:
-		return films[titles.index(title)]
+	if film_title in film_titles:
+		return film_titles[film_titles.index(film_title)]
 	# Check lower
-	if title.lower() in lowercase_titles:
-		return films[lowercase_titles.index(title.lower())]
+	if film_title.lower() in lowercase_titles:
+		return film_titles[lowercase_titles.index(film_title.lower())]
 	# film not found
 	return None
