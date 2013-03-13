1776e65cf4725f30c19c3421f9502fc20769b059
Gabriel Peal <gpeal@u.northwestern.edu>
2013-03-12 19:23:28 -0500

    BUG FIX: handle more odd cases in IMDb

--- adapters/imdb.py
+++ adapters/imdb.py
@@ -15,15 +15,16 @@ class IMDbAdapter(Adapter):
     title = title.replace('-', '')
     r = requests.get('http://imdbapi.org/?q=%22' + title + '%22&limit=100')
     movies = json.loads(r.text)
-    if 'code' in movies.keys() and movies['code'] == 404:
+    if isinstance(movies, dict) and movies['code'] == 404:
       raise FilmNotFoundError(title + ' not found on IMDb')
-    movies = [movie for movie in movies if (movie['title'].replace('-', '').lower() == title.lower())]
+    import pdb; pdb.set_trace()
+
+    # pick out the desired movie from the list of returned movies
+    # NOTE: returned movies don't always have a title or a ranking
+    movies = [movie for movie in movies if ('title' in movie.keys() and 'rating' in movie.keys()and movie['title'].replace('-', '').lower() == title.lower())]
     if movies:
       # make sure the movie has a rating (it may not like in the case of '21 And Over')
-      if 'rating' in movies[0].keys():
-        return movies[0]['rating'] / 10.0
-      else:
-        raise FilmNotFoundError(title + ' has no rating on IMDb')
+      return movies[0]['rating'] / 10.0
     else:
       raise FilmNotFoundError(title + ' not found on IMDb')
 
