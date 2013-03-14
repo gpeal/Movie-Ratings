12be90efcf54da53bdfbb5d830e23c221f245698
Gabriel Peal <gpeal@u.northwestern.edu>
2013-03-13 19:10:59 -0500

    FEATURE: main now asks the user for a movie, promts them for scores for similar films, creates a full profile for each, and outputs the results

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
+from adapters import FilmNotFoundError
 from adapters.imdb import IMDbAdapter
 from adapters.rotten_tomatoes import RTAdapter
 from adapters.metacritic import MetacriticAdapter
-import profiler
-
 import csv
+import profiler
+import similar
+
+# takes a list and returns the same list with duplicates removed
+def unique(seq):
+  seen = set()
+  seen_add = seen.add
+  return [ x for x in seq if x not in seen and not seen_add(x)]
 
 
 def prompt_user():
+  imdb = IMDbAdapter()
+  rt = RTAdapter()
+  metacritic = MetacriticAdapter()
+
+  similar_films = []
   film = raw_input('Enter Movie: ')
-  with open(film + '.csv', 'wb') as csv_output_file:
-    csv_output = csv.DictWriter(csv_output_file, delimiter=',', fieldnames=['Title', 'IMDb', 'Metacritic', 'Rotten Tomatoes', 'User'])
-    csv_output.writeheader()
-    # create the profile for the film they entered
-    profile = profiler.create_profile(film, [metacritic, imdb, rotten_tomatoes])
+  profile = profiler.create_profile(film, [metacritic, rt, imdb])
+  # get the 5 most similar films
+  similar_films.extend(similar.get_similar_film_titles(profile.film))
 
-    # create a profile for 5 similar films to the one the user chose
-    similar_film_titles = profile.get_similar_film_titles()
-    similar_film_profiles = []
-    for similar_film_title in similar_film_titles:
-      new_similar_film_profile = profiler.create_profile(similar_film_title, [metacritic, imdb, rotten_tomatoes])
-      similar_film_profiles.append(new_similar_film_profile)
-      print new_similar_film_profile
+  # get similar films for each of the similar films)
+  # Save the second round of similar films into a new array. Otherwise, we will extend
+  # the array we are iterating through and it will never end (and go in loops)
+  new_similar_films = []
+  for film in similar_films:
+    new_similar_films.extend(similar.get_similar_film_titles(film))
+  similar_films.extend(new_similar_films)
+  #uniquify the list
+  similar_films = unique(similar_films)
 
-    # for each similar film profile, get its similar films so we can get 30 total instead of 5
-    for similar_film_profile in similar_film_profiles:
-      new_similar_film_titles = similar_film_profile.get_similar_film_titles()
-      # if the new similar title isn't in the list of similar titles, add it to the list and create its profile
-      # keeping a list of titles helps ensure we don't create the same profile twice
-      for new_similar_film_title in new_similar_film_titles:
-        if not new_similar_film_title in similar_film_titles:
-          similar_film_titles.append(new_similar_film_title)
-          new_similar_film_profile = profiler.create_profile(new_similar_film_title, [metacritic, imdb, rotten_tomatoes])
-          similar_film_profiles.append(new_similar_film_profile)
-          print new_similar_film_profile
+  # prompt the user to enter a score for each movie
+  profiles = []
+  print 'Enter all score 0-100'
+  print 'Enter -1 to skip movie'
+  print 'Enter -2 to stop collecting'
+  for film in similar_films:
+    score = float(raw_input('Enter score for ' + film + ': '))
+    if score == -1:
+      continue
+    elif score == -2:
+      break
 
-    # write all of the similar profiles to the csv file
-    for similar_film_profile in similar_film_profiles:
-      csv.writerow(similar_film_profile.to_dict())
+    profile = profiler.FilmProfile(film)
+    profile.add_score('user', score / 100)
+    profiles.append(profile)
 
-    import pdb; pdb.set_trace()
+  # populate the scores from the other adapters
+  for profile in profiles:
+    for adapter in [metacritic, rt, imdb]:
+      try:
+        score = adapter.get_film_score(profile.film)
+      except FilmNotFoundError:
+        score = None
+      profile.add_score(str(adapter), score)
+
+  # write the results to a csv
+  csv_file = csv.DictWriter(open('output.csv', 'wb'), delimiter=',', fieldnames=['Title', 'user', 'IMDb', 'Metacritic', 'Rotten Tomatoes'])
+  csv_file.writeheader()
+  for profile in profiles:
+    csv_file.writerow(profile.to_dict())
+
+
+
+
 
 
 
